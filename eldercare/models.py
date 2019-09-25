a4f82a05a032f86a3ddd8daf5d453081b6788c89
asuozzo <asuozzo@gmail.com>
2019-09-25 10:30:33 -0400

    update models

--- eldercare/models.py
+++ eldercare/models.py
@@ -1,16 +1,98 @@
 from django.db import models
 
+
 # Create your models here.
 class Facility(models.Model):
+
+    class Meta:
+        verbose_name_plural = "facilities"
+
     name = models.CharField(max_length=200)
-    care_type = models.CharField(max_length=100)
-    id_num = models.IntegerField(blank=True)
-    cacpacity = models.IntegerField(blank=True)
+
+    TYPE_CHOICES = (
+        ("RCF","Residential Care Facility"),
+        ("ALR","Assisted Living Residence")
+    )
+    care_type = models.CharField(
+        max_length=100,
+        choices=TYPE_CHOICES
+    )
+    id_num = models.CharField(max_length=200,blank=True)
+    capacity = models.IntegerField(blank=True, null=True)
     level = models.IntegerField(blank=True, null=True)
     address = models.CharField(max_length=200)
     town = models.CharField(max_length=200)
     county = models.CharField(max_length=200)
     in_business = models.BooleanField(blank=True, null=True)
-    capacity = models.IntegerField(blank=True)
-    accs = models.BooleanField(blank=True, null=True)
-    erc = models.BooleanField(blank=True, null=True)
+    accs = models.BooleanField(default=False)
+    erc = models.BooleanField(default=False)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Inspection(models.Model):
+    facility = models.ForeignKey(
+        Facility,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    inspection_type = models.CharField(max_length=200, blank=True, null=True)
+    date = models.DateField(blank=True,null=True)
+    documentcloud_url = models.URLField(blank=True, null=True)
+    state_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return ("{0} - {1} {2}".format(self.facility.name, self.inspection_type, self.date))
+
+
+class Complaint(models.Model):
+    facility = models.ForeignKey(
+        Facility,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+
+    intake_id = models.CharField(
+        max_length=200, unique=True, blank=True, null=True
+    )
+    received_start_date = models.DateField()
+
+    related_inspection = models.ForeignKey(
+        Inspection,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.intake_id
+
+class Allegation(models.Model):
+    facility = models.ForeignKey(
+        Facility, on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+    complaint = models.ForeignKey(
+        Complaint, on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+    allegation_num = models.IntegerField(blank=True, null=True)
+    category = models.CharField(max_length=300, blank=True, null=True)
+    subcategory = models.CharField(max_length=300, blank=True, null=True)
+    seriousness = models.CharField(max_length=300, blank=True, null=True)
+    substantiation = models.CharField(max_length=300, blank=True, null=True)
+    finding = models.CharField(max_length=300, blank=True, null=True)
+
+    def __str__(self):
+        return ("{0}-{1}".format(self.complaint.intake_id, self.allegation_num))
+
+class Penalties(models.Model):
+    facility = models.ForeignKey(
+        Facility, on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+    date = models.DateField()
+    penalty = models.IntegerField(blank=True, null=True)
