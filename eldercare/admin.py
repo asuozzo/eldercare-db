510fc5d161d4d0c33024df20851f06971969bee2
Andrea Suozzo <asuozzo@gmail.com>
2019-11-05 18:46:26 -0500

    update inspection, citation display

--- eldercare/admin.py
+++ eldercare/admin.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
 
-from .models import Facility, Inspection, Allegation, Complaint, Penalty
+from .models import (
+    Facility, Inspection, Allegation, 
+    Complaint, Penalty, Citation,Severity_Scope
+)
 
 
 # Register your models here.
 class FacilityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'care_type', 'capacity', 'town', 'county')
+    list_display = ('name', 'care_type', 'capacity', 'town', 'county','score')
     search_fields = ('name',)
 
 admin.site.register(Facility, FacilityAdmin)
 
 class InspectionAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'facility', 'inspection_type', 'date')
-    search_fields = ('facility__name', 'inspection_type', 'date', 'documentcloud_url')
+    list_display = ('__str__', 'facility','inspection_type', 'date')
+    search_fields = ('facility__name', 'slug', 'inspection_type', 'date', 'documentcloud_url')
 
 admin.site.register(Inspection, InspectionAdmin)
 
@@ -33,3 +36,10 @@ class PenaltyAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'date', 'penalty')
 
 admin.site.register(Penalty, PenaltyAdmin)
+
+class CitationAdmin(admin.ModelAdmin):
+    list_display = ('citation_num', 'inspection', 'severity_scope')
+
+admin.site.register(Citation, CitationAdmin)
+
+admin.site.register(Severity_Scope)
