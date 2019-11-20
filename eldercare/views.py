e48bf414a045e0a0cb48d4491cf458c7daaee421
asuozzo <asuozzo@gmail.com>
2019-11-20 09:46:51 -0500

    implement vue main page

--- eldercare/views.py
+++ eldercare/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.db.models import Sum, Func, Count
 from django.core.serializers.json import DjangoJSONEncoder
 import json
+import pandas as pd
 from django.db import models
 from bakery.views import BuildableDetailView, BuildableListView
 
@@ -9,10 +10,6 @@ from .models import Facility, Complaint
 
 class FacilityListView(BuildableListView):
     model = Facility
-
-    ## standard template
-    # queryset = Facility.objects.order_by()
-    # template_name = 'eldercare/index_old.html'
 
     ## vue template
     template_name = 'eldercare/index.html'
@@ -33,7 +30,8 @@ class FacilityListView(BuildableListView):
                 "type":facility.care_type,
                 "town":facility.town,
                 "status":facility.in_business,
-                "county":facility.county
+                "county":facility.county,
+                "slug":facility.slug
             }
             facilities_json.append(facility_dict)
         context["facilities_json"] = json.dumps(facilities_json,cls=DjangoJSONEncoder)
@@ -52,7 +50,8 @@ class FacilityDetailView(BuildableDetailView):
         context["inspections"] = facility.inspection_set.all()
         context["scores"] = list(Facility.objects.values_list('score', flat=True))
         citation_summary = facility.citation_set.annotate(year=Year('inspection__date')).values('year','severity_scope__letter').annotate(Count('severity_scope__letter'))
-        context["citation_type_json"] = json.dumps(list(citation_summary), cls=DjangoJSONEncoder)
+        citations_dict = citations_to_df(citation_summary)
+        context["citation_type_json"] = citations_dict
         citation_totals = facility.citation_set.annotate(year=Year('inspection__date')).values('year').annotate(Count('severity_scope'))
         context["citation_json"] = json.dumps(list(citation_totals), cls=DjangoJSONEncoder)
         context["complaints_count"] = facility.allegation_set.count()
@@ -70,3 +69,29 @@ def num_or_zero(num):
     if num == None:
         return 0
     return num
+
+def citations_to_df(citation_summary):
+    try:
+        severity_code = {
+            "A":1,
+            "B":1,
+            "C":1,
+            "D":2,
+            "E":2,
+            "F":2,
+            "G":3,
+            "H":3,
+            "I":3,
+            "J":4,
+            "K":4,
+            "L":4
+        }
+        citations = pd.DataFrame(citation_summary)
+        citations.rename(columns={"severity_scope__letter__count":"count"}, inplace=True)
+        citations["severity_code"] = citations["severity_scope__letter"].map(severity_code)
+        citations = citations[["year","severity_code","count"]]
+        citations = citations.groupby(["year","severity_code"]).sum().reset_index()
+
+        return json.dumps(citations.to_dict("records"))
+    except KeyError:
+        return json.dumps({})
